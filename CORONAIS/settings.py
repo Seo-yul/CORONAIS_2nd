d268aa8c9f65a07120b3891913bc8b3ae7fcccc2
resourcesaga <0103.seoyul@gmail.com>
2020-09-18 10:21:26 +0000

    HOST 수정

--- CORONAIS/settings.py
+++ CORONAIS/settings.py
@@ -26,7 +26,7 @@ with open(os.path.join(BASE_DIR,'www_dir','secret_key.txt')) as f:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
